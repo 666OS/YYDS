54fdef66e1a02b4ceb187a633d1f5d3aa47051cb
GitHub Action <action@github.com>
2025-01-01 23:14:00 +0800

    Auto Update 2025-01-01 23:14

--- surge/scripts/Pinduoduo.js
+++ surge/scripts/Pinduoduo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/PinDuoDuo/PinDuoDuo_remove_ads.js
-// 更新时间：2025-01-01 09:38:47
+// 更新时间：2025-01-01 20:35:38
 // 脚本引用 https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/pdd.js
 // 2024-11-06 04:20
 
